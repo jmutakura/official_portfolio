f0f2b7e48612cce7e2ff368e8c9583b08cdb678d
jmutakura <jmutakura@gmail.com>
2023-03-14 18:23:17 -0600

    Using app directory

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='en'>
+			<ChakraProvider>
+				<body>{children}</body>
+			</ChakraProvider>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { Heading } from '@chakra-ui/react';
+import Image from 'next/image';
+
+export default function Home() {
+	return <Heading> Portfolio Site</Heading>;
+}
