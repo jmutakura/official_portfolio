fcbbc886c9b8c4d92735fe250665b9bb060218d6
Jonathan Mutakura <46237043+jmutakura@users.noreply.github.com>
2023-12-13 18:02:40 -0600

    Update work.ts with recent work experience

--- data/content/work.ts
+++ data/content/work.ts
@@ -2,6 +2,12 @@ import { Work } from 'types';
 
 const work: Work[] = [
 	{
+		title: 'Software Developer - Western Ag Innovations',
+		startDate: 'May 2023',
+		endDate: 'Present',
+		desc: 'As a software developer at Western Ag Innovations, I work with mysql, php, html & CSS on various websites, developing in Java for the android platform, working in dart & flutter for mobile apps and also consultng with various stakeholders.',
+	},
+	{
 		title: 'Junior Software Engineer - Cadence',
 		startDate: 'December 2022',
 		endDate: 'March 2023',
